fce794aa1014c90115a5b0a93edb90a9ca89f55f
superwhiskers <whiskerdev@protonmail.com>
2024-07-15 08:50:43 -0400

    adjustments to the interface and the readme

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # shepherd
 
-Describe your project here.
+a tool for simulating [flock](https://github.com/superwhiskers/flock). it's intended
+to be used to test out different feed generation / ranking algorithms, but it may be
+useful for other things
--- src/shepherd/__init__.py
+++ src/shepherd/__init__.py
@@ -1,15 +1,37 @@
+from typing import Optional
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer
+
+
 from shepherd.simulation import Flock, FlockSettings
 from shepherd.feed import Response
 from shepherd.shepherd.dummy import Dummy as DummyShepherd
 from shepherd.sheep.dummy import Dummy as DummySheep
 
 
+class ShepherdApp(App[None]):
+    """The shepherd flock simulator application"""
+
+    flock: Optional[Flock] = None
+
+    def compose(self) -> ComposeResult:
+        """Builds child widgets for the application"""
+
+        yield Header()
+        yield Footer()
+
+
 def main():
+    """
     flock = Flock([DummyShepherd()], [DummySheep()], FlockSettings())
 
     for _ in range(20):
         flock.simulate_epoch()
 
     print(flock.tags)
+    """
+
+    app = ShepherdApp()
+    app.run()
 
     return 0
